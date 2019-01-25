13662d3913ca56eede5aa40da7efb54c934aad40
dbenoot <dbenoot@gmail.com>
2019-01-25 20:37:18 +0100

    test struct defined

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+
+)
+
+func main () {
+
+  type Event struct {
+  	Name        string
+  	Description string
+  	Major       bool
+  	ImpactShip  map[string]int
+  	ImpactAstronaut    string
+  	ImpactLocation  map[string]int
+  }
+
+}
